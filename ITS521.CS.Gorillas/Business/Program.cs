1b37f628941048d0596acc0580d691d08377bed9
Hansol Jo <hansol.jo@gmail.com>
2020-02-25 23:56:25 -0500

    Initial commit

---
+++ ITS521.CS.Gorillas/Business/Program.cs
@@ -0,0 +1,20 @@
+﻿using ITS521.CS.Gorillas.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITS521.CS.Gorillas.Business {
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
